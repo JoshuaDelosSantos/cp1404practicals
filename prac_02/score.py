156275f41e0e32a22743658414f3158ece1e1745
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-09-15 21:40:23 +1000

    Refactor result determine from score program to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,13 +3,30 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
+import random
 
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Pass")
-else:
-    print("Bad")
+
+def main():
+    score = float(input("Enter score: "))
+    result = determine_result(score)
+    print(result)
+
+    random_score = random.randint(1, 100)
+    random_score_result = determine_result(random_score)
+    print(random_score_result)
+
+
+def determine_result(score):
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Pass"
+    else:
+        result = "Bad"
+
+    return result
+
+
+main()
