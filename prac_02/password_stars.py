630e34d9e2452b508cd092b129f4907cab17da3e
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-09-15 21:42:33 +1000

    Add docstrings

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,15 +2,18 @@ minimum_password_length = 5
 
 
 def main():
+    """Get password from user and print stars according to the length of password"""
     password = get_password()
     print_star(password)
 
 
 def print_star(password):
+    """Print star according to the length of password"""
     print("*" * len(password))
 
 
 def get_password():
+    """Get a password that is not and empty string"""
     password = input("Enter password: ")
     while len(password) < minimum_password_length:
         print("Password not long enough!")
