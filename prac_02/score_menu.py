cc29ad6d31ab3a1e24294e4f514a0814b6b4d034
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-09-16 12:29:14 +1000

    Add score menu program with an imported function from score.py

---
+++ prac_02/score_menu.py
@@ -0,0 +1,38 @@
+from score import determine_result
+
+MENU = "(G)et a valid score\n(P)rint result\n(S)how stars\n(Q)uit"
+
+
+def main():
+    score = get_valid_score()
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "G":
+            score = get_valid_score()
+        elif choice == "P":
+            result = determine_result(score)
+            print(result)
+        elif choice == "S":
+            print_stars(score)
+        else:
+            print("Invalid choice")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Farewell")
+
+
+def get_valid_score():
+    score = int(input("Score: "))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = int(input("Score: "))
+    return score
+
+
+def print_stars(score):
+    print("*" * score, end=' ')
+    print()
+
+
+main()
