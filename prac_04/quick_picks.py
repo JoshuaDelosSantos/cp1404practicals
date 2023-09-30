c87485b306511fc6a390dc877c8bda3610527779
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-10-01 01:08:48 +1000

    Add list comprehension version of displaying numbers

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -2,18 +2,21 @@ import random
 
 LOW_NUMBER = 1
 HIGH_NUMBER = 45
-NUMBER_OF_LINES = 6
+NUMBER_PER_LINE = 6
 
 quick_picks = int(input("How many quick picks? "))
 
 for i in range(quick_picks):
     numbers = []
-    for j in range(NUMBER_OF_LINES):
+    for j in range(NUMBER_PER_LINE):
         number = random.randint(LOW_NUMBER, HIGH_NUMBER)
         while number in numbers:
             number = random.randint(LOW_NUMBER, HIGH_NUMBER)
         numbers.append(number)
     numbers.sort()
-    for number in numbers:
-        print(f"{number:2}", end=' ')
-    print()
+    print(" ".join(f"{number:2}" for number in numbers))
+
+    # Another version:
+    # for number in numbers:
+    #     print(f"{number:2}", end=' ')
+    # print()
