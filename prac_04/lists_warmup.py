9935153f974fd8024f9cc20d85381b03c7acc4b3
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-09-30 19:16:49 +1000

    Add Q1-Q4 answers

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,6 +1,5 @@
 numbers = [3, 1, 4, 1, 5, 9, 2]
 
-
 #  numbers[0] = 3
 #  numbers[-1] = 2
 #  numbers[3] = 1
@@ -11,3 +10,15 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 #  "3" in numbers = False
 #  numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 
+
+# 1
+# numbers[0] = "Ten"
+
+# 2
+# numbers[-1] = 1
+
+# 3
+# numbers[2:]
+
+# 4
+# 9 in numbers
