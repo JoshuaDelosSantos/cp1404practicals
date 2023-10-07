13bd74d3b2e04178f3d37de73b7dfdb9dd5c3f04
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-10-07 21:02:30 +1000

    Refactor output code to ensure a neat presentation

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,17 +1,18 @@
 """
 Word Occurrences
 Estimate: 20 minutes
-Actual:
+Actual: 31 minutes :(
 """
 word_to_count = {}
 
 text = input("Text: ")
 words = text.split(" ")
 unique_words = set(words)
+longest_word = max(len(word) for word in words)
 
 for word in words:
     word_to_count[word] = word_to_count.get(word, 0) + 1
 
-for unique_word in unique_words:
-    print(f"{unique_word} : {word_to_count[unique_word]}")
+for unique_word in sorted(unique_words):
+    print(f"{unique_word:{longest_word}} : {word_to_count[unique_word]}")
 
