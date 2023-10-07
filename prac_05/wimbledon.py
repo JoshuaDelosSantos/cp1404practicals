526ac5059606c132e723bfe037849a11394e52ef
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-10-07 21:59:44 +1000

    Add initial function structure for wimbledon program

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,21 @@
+"""
+Wimbledon
+Estimate: 40 minutes
+Actual:
+"""
+
+
+def main():
+    pass
+
+
+def load_from_file():
+    pass
+
+
+def process_data():
+    pass
+
+
+def display_information():
+    pass
