8026b52552c0d37a64c13286efafdc173545f2e8
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-10-07 20:55:18 +1000

    Add code for displaying initial output

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -8,7 +8,10 @@ word_to_count = {}
 text = input("Text: ")
 words = text.split(" ")
 unique_words = set(words)
+
 for word in words:
     word_to_count[word] = word_to_count.get(word, 0) + 1
 
-print(word_to_count)
+for unique_word in unique_words:
+    print(f"{unique_word} : {word_to_count[unique_word]}")
+
