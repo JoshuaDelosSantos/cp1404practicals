bce007f312311fcc1044f061368abd1345eb70d9
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-10-07 22:25:44 +1000

    Add file loading code

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -4,18 +4,26 @@ Estimate: 40 minutes
 Actual:
 """
 
+FILENAME = "wimbledon.csv"
+
 
 def main():
-    pass
+    data = load_from_file()
+    year_to_match = process_data(data)
 
 
 def load_from_file():
-    pass
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()  # Ignore title
+        return [line.strip() for line in in_file.readlines()]
 
 
-def process_data():
+def process_data(data):
     pass
 
 
 def display_information():
     pass
+
+
+main()
