8fc1f144b1fd6c91444905aaa122a314b621b91f
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-10-07 21:39:54 +1000

    Add initial code for emails program

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -6,8 +6,26 @@ Actual:
 
 
 def main():
-    pass
+    email_to_name = {}
+
+    email = input("Email: ")
+    while email != "":
+        name = extract_name(email)
+        email_to_name[email] = name
+        choice = input(f"Is your name {name}? (Y/n)").upper()
+
+        if choice != "Y" and choice != "":
+            new_name = input("Name: ")
+            email_to_name[email] = new_name
+        email = input("Email: ")
+
+    for email in email_to_name:
+        print(f"{email_to_name[email]} {email}")
 
 
-def extract_name():
-    pass
+def extract_name(email):
+    parts = email.split('@')
+    return parts[0]
+
+
+main()
