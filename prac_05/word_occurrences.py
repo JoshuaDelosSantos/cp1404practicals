71d5a59c03200a3bb2be04d26e456fc8726c9636
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-10-11 15:28:36 +1000

    Refactor prac_05 word_occurrences program print statement for efficiency

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -7,12 +7,11 @@ word_to_count = {}
 
 text = input("Text: ")
 words = text.split(" ")
-unique_words = set(words)
 longest_word = max(len(word) for word in words)
 
 for word in words:
     # Add the count of the occurrences of the word
     word_to_count[word] = word_to_count.get(word, 0) + 1
 
-for unique_word in sorted(unique_words):
-    print(f"{unique_word:{longest_word}} : {word_to_count[unique_word]}")
+for word, count in sorted(word_to_count.items()):
+    print(f"{word:{longest_word}} : {count}")
