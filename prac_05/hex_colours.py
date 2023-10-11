2a75a56f6e8a48684ef108abe977fdc7d3b1cf97
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-10-11 15:17:43 +1000

    Refactor variable name 'colour' to 'colour_name' for readability in prac_05 hex_colours program

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -2,13 +2,13 @@ COLOUR_TO_HEXADECIMAL = {"Absolute Zero": "#0048ba", "Baby Blue": "#89cff0", "Ca
                          "Dandelion": "#f0e130", "Ebony": "#555d50", "Fawn": "#e5aa70", "Ginger": "#b06500",
                          "Heliotrope": "#df73ff", "Iceberg": "#71a6d2", "Inchworm": "#b2ec5d"}
 
-colour = input("Enter a colour: ").title()
-while colour != "":
+colour_name = input("Enter a colour: ").title()
+while colour_name != "":
     try:
-        print(f"{COLOUR_TO_HEXADECIMAL[colour]}")
+        print(f"{COLOUR_TO_HEXADECIMAL[colour_name]}")
     except KeyError:
         print("That colour is not in the dictionary yet!")
 
-    colour = input("Enter a colour: ").title()
+    colour_name = input("Enter a colour: ").title()
 
 print("Finished.")
