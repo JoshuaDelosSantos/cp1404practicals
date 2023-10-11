c5904a20e31e1a865f0137f0e2c03ae9691f3caa
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-10-11 11:19:51 +1000

    Refactor code format for prac_05 hex_colours to make it look neat

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,7 +1,6 @@
-COLOUR_TO_HEXADECIMAL = {"Absolute Zero": 	"#0048ba", "Baby Blue": "#89cff0", "Cardinal": "#c41e3a",
-                          "Dandelion": "#f0e130", "Ebony": "#555d50", "Fawn": "#e5aa70", "Ginger": "#b06500",
-                          "Heliotrope": "#df73ff", "Iceberg": "#71a6d2", "Inchworm": "#b2ec5d"}
-
+COLOUR_TO_HEXADECIMAL = {"Absolute Zero": "#0048ba", "Baby Blue": "#89cff0", "Cardinal": "#c41e3a",
+                         "Dandelion": "#f0e130", "Ebony": "#555d50", "Fawn": "#e5aa70", "Ginger": "#b06500",
+                         "Heliotrope": "#df73ff", "Iceberg": "#71a6d2", "Inchworm": "#b2ec5d"}
 
 colour = input("Enter a colour: ").title()
 while colour != "":
@@ -13,4 +12,3 @@ while colour != "":
     colour = input("Enter a colour: ").title()
 
 print("Finished.")
-
