813f516daf322dcec06f297b6b70638eed34c5db
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-10-11 11:11:34 +1000

    Add inline comment for prac_05 world_occurrences program for the line of code that counts the occurrence of that word in the dictionary

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -11,8 +11,8 @@ unique_words = set(words)
 longest_word = max(len(word) for word in words)
 
 for word in words:
+    # Add the count of the occurrences of the word
     word_to_count[word] = word_to_count.get(word, 0) + 1
 
 for unique_word in sorted(unique_words):
     print(f"{unique_word:{longest_word}} : {word_to_count[unique_word]}")
-
