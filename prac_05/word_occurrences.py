2b51f16dfe54482f4008bb23af76e72ffdcf9c39
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-10-11 16:02:03 +1000

    Refactor inline comment in prac_05 word_occurrences program

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -9,8 +9,8 @@ text = input("Text: ")
 words = text.split()
 longest_word = max(len(word) for word in words)
 
+# Tally the count of word in words
 for word in words:
-    # Add the count of the occurrences of the word
     word_to_count[word] = word_to_count.get(word, 0) + 1
 
 for word, count in sorted(word_to_count.items()):
