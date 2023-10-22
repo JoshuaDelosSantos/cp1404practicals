b133a91db1e14037a9ddac543204ab606544852c
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-10-22 17:04:09 +1000

    Add code for __init__ method for guitar.py

---
+++ prac_06/guitar.py
@@ -0,0 +1,14 @@
+"""
+prac_06 - guitar.py
+
+Estimated time: 30 mins
+Actual time:
+"""
+
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise Guitar instance"""
+        self.name = name
+        self.year = year
+        self.cost = cost
