8a4fd59bc56272f5d3be2771c9c1984563911df2
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-10-22 17:33:19 +1000

    Add code for is_vintage method for formatting in guitar.py

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -7,7 +7,6 @@ Actual time: 10 mins
 
 
 class Guitar:
-    current_year = 2023
 
     def __init__(self, name="", year=0, cost=0):
         """Initialise Guitar instance"""
@@ -17,12 +16,12 @@ class Guitar:
 
     def __str__(self):
         """Return string representation of data for Guitar"""
-        return f"{self.name} ({self.year}) : ${self.cost}"
+        return f"{self.name} ({self.year}) : ${self.cost:,}"
 
-    def get_age(self, current_year):
+    def get_age(self, current_year=2023):
         """Return age of guitar in current year from year of manufacture"""
         return current_year - self.year
 
-    def is_vintage(self, current_year):
+    def is_vintage(self, current_year=2023):
         """Determine if guitar age is >= 50"""
         return current_year - self.year >= 50
