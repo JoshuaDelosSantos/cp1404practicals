bbacd3ca7b7d4ca59678ecaa87f1ad7b1c91cad1
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-10-22 16:14:45 +1000

    Add code for __str__ method in programming_language.py

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -8,13 +8,22 @@ Actual time:
 
 class ProgrammingLanguage:
     """Represent a programming language object"""
-    def __init__(self, field="", typing="", reflection=True, year=0):
+
+    def __init__(self, field="", typing="".title(), reflection=True, year=0):
         """Initialise programming language instance"""
         self.field = field
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
+    def __str__(self):
+        """Return string representation of data regarding the ProgrammingLanguage"""
+        return f"{self.field}, {self.typing} Typing, Reflection={self.reflection}," \
+               f" First appeared in {self.year}"
+
     def is_dynamic(self):
         """Determine if programming language is dynamically typed"""
-        pass
+        if self.typing == "Dynamic":
+            return True
+        else:
+            return False
