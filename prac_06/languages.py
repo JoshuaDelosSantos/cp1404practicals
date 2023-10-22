4d3b7e1e179ef9b915d94133e2f3fc35a80dc430
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-10-22 16:15:19 +1000

    Add code for languages.py

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -9,4 +9,10 @@ from prac_06.programming_language import ProgrammingLanguage
 
 
 def main():
-    pass
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+
+
+main()
