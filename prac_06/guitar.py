c3b949a89bb318a73f69405a876ca078a257b65e
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-10-22 17:09:31 +1000

    Add code for get_age method in guitar.py

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -7,6 +7,8 @@ Actual time:
 
 
 class Guitar:
+    current_year = 2023
+
     def __init__(self, name="", year=0, cost=0):
         """Initialise Guitar instance"""
         self.name = name
@@ -16,3 +18,7 @@ class Guitar:
     def __str__(self):
         """Return string representation of data for Guitar"""
         return f"{self.name} ({self.year}) : ${self.cost}"
+
+    def get_age(self, current_year):
+        """Return age of guitar in current year from year of manufacture"""
+        return current_year - self.year
