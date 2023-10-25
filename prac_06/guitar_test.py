f4c63fdcfc5fba6c6157ac175d86bc13d1c23c91
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-10-25 12:35:55 +1000

    Refactor variable name from another_guitar to other_guitar

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -7,22 +7,19 @@ Actual time: 10 mins
 
 from prac_06.guitar import Guitar
 
-CURRENT_YEAR = 2023
-
 
 def main():
     """Test code for Guitar class"""
     my_guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
-    my_guitar_age = my_guitar.get_age(CURRENT_YEAR)
-    another_guitar = Guitar("Another guitar", 2014, 5)
+    my_guitar_age = my_guitar.get_age()
+    other_guitar = Guitar("Another guitar", 2014, 5)
 
     print(my_guitar)
 
     print(f"{my_guitar.name} get_age() - Expected 101. Got {my_guitar_age}")
-    print(f"{another_guitar.name} get_age() - Expected 9. Got {another_guitar.get_age(CURRENT_YEAR)}")
-    print(f"{my_guitar.name} is_vintage() - Expected True. Got {my_guitar.is_vintage(CURRENT_YEAR)}")
-    print(f"{another_guitar.name} is_vintage() - Expected False. Got {another_guitar.is_vintage(CURRENT_YEAR)}")
-
+    print(f"{other_guitar.name} get_age() - Expected 9. Got {other_guitar.get_age()}")
+    print(f"{my_guitar.name} is_vintage() - Expected True. Got {my_guitar.is_vintage()}")
+    print(f"{other_guitar.name} is_vintage() - Expected False. Got {other_guitar.is_vintage()}")
 
 
 main()
