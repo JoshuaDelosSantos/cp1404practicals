2561a07f13791b6443f682df7548903c364ffad7
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-10-28 19:09:08 +1000

    Refactor generator object variable name in guitars.py

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -13,8 +13,8 @@ def main():
     guitars = []
 
     print("My guitars!")
-    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
     name = input("Name: ")
     while name != "":
         year = int(input("Year: "))
@@ -26,14 +26,13 @@ def main():
 
     print(f"\n... snip ...\n")
 
-    # guitar_names = [Guitar(name).name for name in guitars]
-    longest_name = max(len(guitar.name) for guitar in guitars)
+    longest_name_length = max(len(guitar.name) for guitar in guitars)
 
     for i, guitar in enumerate(guitars, 1):
         my_guitar = guitar
         vintage_string = "" if my_guitar.is_vintage() else "(vintage)"
-        print(f"Guitar {i}: {my_guitar.name:>{longest_name}} ({my_guitar.year}), "
-              f"worth $ {my_guitar.cost:,} {vintage_string}")
+        print(f"Guitar {i}: {my_guitar.name:>{longest_name_length}} ({my_guitar.year}), "
+              f"worth $ {my_guitar.cost:,.2f} {vintage_string}")
 
 
 main()
