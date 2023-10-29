b0fbd13b7bf7fa49571f117288a1029a85953c17
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-10-29 13:03:20 +1000

    Add guitar.py to prac_07

---
+++ prac_07/guitar.py
@@ -0,0 +1,27 @@
+"""
+prac_07 - guitar.py
+"""
+
+CURRENT_YEAR = 2023
+VINTAGE_AGE = 50
+
+
+class Guitar:
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise Guitar instance"""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return string representation of data for Guitar"""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Return age of guitar in current year from year of manufacture"""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Determine if a Guitar is vintage by age """
+        return CURRENT_YEAR - self.year >= VINTAGE_AGE
