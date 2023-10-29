29b4698054c683ba30bc49160b2d9646278e733b
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-10-29 15:41:58 +1000

    Add skeleton function structure for project_management.py

---
+++ prac_07/project_management.py
@@ -0,0 +1,33 @@
+"""
+prac_07 - project_management.py
+
+Estimated time: 90 mins
+Actual time:
+"""
+
+
+def main():
+    pass
+
+
+def load_projects():
+    pass
+
+
+def save_projects():
+    pass
+
+
+def display_projects():
+    pass
+
+
+def add_new_project():
+    pass
+
+
+def update_project():
+    pass
+
+
+main()
