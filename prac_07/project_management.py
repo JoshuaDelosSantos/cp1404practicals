5c4fe4f63c2163d8a691fb5483c712f8557fb4ee
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-10-29 15:57:42 +1000

    Add menu pattern code for project_management.py

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -5,13 +5,44 @@ Estimated time: 90 mins
 Actual time:
 """
 
+import datetime
+from prac_07.project import Project
+
+MENU = ("(L)oad projects\n(S)ave projects\n(D)isplay projects\n(F)ilter projects by date\n"
+        "(A)dd new project\n(U)pdate project\n(Q)uit")
+
 
 def main():
-    pass
+    print(MENU)
+
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+            load_projects()
+        elif choice == "S":
+            save_projects()
+        elif choice == "D":
+            display_projects()
+        elif choice == "F":
+            filter_projects()
+        elif choice == "A":
+            add_new_project()
+        elif choice == "U":
+            update_project()
+        else:
+            print("Invalid choice!")
+            choice = input(">>> ").upper()
+        print(MENU)
 
 
 def load_projects():
-    pass
+    filename = input("Enter filename(.txt): ")
+
+    while filename == "":
+        print("Filename cannot be blank!")
+        filename = input("Enter filename(.txt): ")
+
+
 
 
 def save_projects():
@@ -19,6 +50,10 @@ def save_projects():
 
 
 def display_projects():
+    pass
+
+
+def filter_projects():
     pass
 
 
