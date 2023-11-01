dcb94aa283fc2b9eba0c79cb6f07febe50328c85
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-11-01 18:50:04 +1000

    Add docstrings to guitar.py

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -9,13 +9,13 @@ VINTAGE_AGE = 50
 class Guitar:
 
     def __init__(self, name="", year=0, cost=0):
-        """Initialise Guitar instance"""
+        """Initialise Guitar instance."""
         self.name = name
         self.year = year
         self.cost = cost
 
-    def __repr__(self):
-        """Return string representation of data for Guitar"""
+    def __str__(self):
+        """Return string representation of data for Guitar."""
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
 
     def __lt__(self, other):
@@ -23,9 +23,9 @@ class Guitar:
         return self.year < other.year
 
     def get_age(self):
-        """Return age of guitar in current year from year of manufacture"""
+        """Return age of guitar in current year from year of manufacture."""
         return CURRENT_YEAR - self.year
 
     def is_vintage(self):
-        """Determine if a Guitar is vintage by age """
+        """Determine if a Guitar is vintage by age."""
         return CURRENT_YEAR - self.year >= VINTAGE_AGE
