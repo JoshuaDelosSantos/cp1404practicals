6ce3997ed70695e80baa857c4a3767399f287281
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-11-02 20:33:57 +1000

    Removed redundant import in project_management.py

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -7,7 +7,6 @@ Actual time: 118 mins
 
 import datetime
 from prac_07.project import Project
-from operator import attrgetter
 
 MENU = ("- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n"
         "- (A)dd new project\n- (U)pdate project\n- (Q)uit")
