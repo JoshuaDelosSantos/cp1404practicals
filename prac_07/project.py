a41e8c62fc5d0b7f05f599e9a64294ddfd43c486
JoshuaDelosSantos <joshuadelossantos.2598@gmail.com>
2023-11-03 15:21:19 +1000

    Refactor __repr__ method to __str__ in project.py

--- prac_07/project.py
+++ prac_07/project.py
@@ -5,17 +5,19 @@ Estimated time: 60 mins
 Actual time: 10 mins
 """
 
+import datetime
+
 
 class Project:
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
         """Initialise Project instance."""
         self.name = name
-        self.start_date = start_date
+        self.start_date = datetime.datetime.strptime(start_date, '%d/%m/%Y').date()
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
 
-    def __repr__(self):
+    def __str__(self):
         """Return string representation of data for Project."""
         return (
             f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate:,.2f}"
